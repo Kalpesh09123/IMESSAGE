2b50f08581f925484d17b0c09429f67ab9918b13
kalpesh vibhute <98274709+Kalpesh09123@users.noreply.github.com>
2026-08-20 11:41:25 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ import path from "path";
 import { clerkMiddleware } from '@clerk/express';
 
 import User from "./models/user.model.js"
-import { connectDB } from '../src/lib/db.js';
+import { connectDB } from '../lib/db.js';
 
 
 const app = express();
